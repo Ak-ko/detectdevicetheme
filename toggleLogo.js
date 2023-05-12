fc325680817d183d599649132f1b51a03ef0e5bc
Aung Khant Ko Ko <85675334+Ak-ko@users.noreply.github.com>
2023-05-12 23:56:35 +0630

    Update toggleLogo.js

--- toggleLogo.js
+++ toggleLogo.js
@@ -1,7 +1,27 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+// Wait for the page to finish loading before running the code
+window.addEventListener('load', function() {
+  // Get references to the two logo images
+  var logoLight = document.getElementById('logo-light');
+  var logoDark = document.getElementById('logo-dark');
+
+  // Check if the device prefers dark mode
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     // User prefers dark color scheme
-    document.getElementById('logo-dark').style.display = 'inline';
-} else {
+    // Hide the light logo and show the dark logo
+    if (logoLight) {
+      logoLight.style.display = 'none';
+    }
+    if (logoDark) {
+      logoDark.style.display = 'inline';
+    }
+  } else {
     // User prefers light color scheme
-    document.getElementById('logo-light').style.display = 'inline';
-}
+    // Hide the dark logo and show the light logo
+    if (logoDark) {
+      logoDark.style.display = 'none';
+    }
+    if (logoLight) {
+      logoLight.style.display = 'inline';
+    }
+  }
+});
